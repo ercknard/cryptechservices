5ec1dbf6894bb70c23d6fe4473b7863998ec27ea
Ercknard Guren <79645928+ercknard@users.noreply.github.com>
2024-12-28 23:14:20 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,21 +10,31 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <meta
-          id="meta-description"
           name="description"
+          id="og:description"
           property="og:description"
           content="We provide web3 and fullstack development services and support, primarily focusing on cryptocurrency , blockchain technology , cyber security , web designing and technical support / consultation."
         />
         <meta
           name="keywords"
-          content="Minetest, CryptechTest, Cryptocurrency, Play2earn, Voxelgame, TESTCoin, BTC, ETH, MRX, SEND, OHMC, SCC"
+          id="og:keywords"
+          property="og:keywords"
+          content="Full-Stack Web Development, Web3 And DAPP Development, Solidity Development, Web Design, Graphic Design, Technical Consultation"
         />
 
-        <meta id="og-title" property="og:title" content="CryptechTest" />
+        <meta id="og-title" property="og:title" content="Cryptech.Services" />
 
-        <meta property="og:url" content="https://cryptech.services" />
+        <meta
+          property="og:url"
+          id="og:url"
+          content="https://cryptech.services"
+        />
 
-        <meta property="og:image" content="/static/images/ctlogo.png" />
+        <meta
+          property="og:image"
+          id="og:image"
+          content="/static/images/ctlogo.png"
+        />
       </Head>
       <ThemeProvider>
         <Component {...pageProps} />
